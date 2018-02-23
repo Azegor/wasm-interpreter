f93885cd7bc6dc6bba36a56165f70922950ba7e1
Azegor <azegor@hotmail.de>
2018-02-24 02:18:03 +0900

    functions section parsing

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate byteorder;
 
 use std::fs::File;
 use std::io::Read;
+use std::io::{Seek, SeekFrom};
 use std::io::BufReader;
 use std::string::String;
 use byteorder::{LittleEndian, ReadBytesExt};
@@ -24,6 +25,14 @@ impl Parser {
         Parser {
             file: BufReader::new(File::open("examples/add.wasm").unwrap()),
         }
+    }
+
+    fn get_current_offset(&mut self) -> u64 {
+        self.file.seek(SeekFrom::Current(0)).unwrap()
+    }
+
+    fn get_read_len(&mut self, old: u64) -> u64 {
+        self.get_current_offset() - old
     }
 
     fn read_byte(&mut self) -> u8 {
@@ -52,7 +61,7 @@ impl Parser {
         self.file.read_i64::<LittleEndian>().unwrap()
     }
 
-    fn read_varuint_len(&mut self, len: i32) -> Option<(u64, u64)> {
+    fn read_varuint_impl(&mut self, len: i32) -> Option<(u64, u64)> {
         let mut res: u64 = 0;
         let mut shift = 0;
         let mut read_bytes: u64 = 0;
@@ -73,10 +82,10 @@ impl Parser {
     }
 
     fn read_varuint(&mut self, len: i32) -> u64 {
-        self.read_varuint_len(len).unwrap().0
+        self.read_varuint_impl(len).unwrap().0
     }
 
-    fn read_varint_len(&mut self, len: i32) -> Option<(i64, u64)> {
+    fn read_varint_impl(&mut self, len: i32) -> Option<(i64, u64)> {
         let mut res: i64 = 0;
         let mut shift = 0;
         let mut read_bytes: u64 = 0;
@@ -101,7 +110,7 @@ impl Parser {
     }
 
     fn read_varint(&mut self, len: i32) -> i64 {
-        self.read_varint_len(len).unwrap().0
+        self.read_varint_impl(len).unwrap().0
     }
 
     fn parse(&mut self) {
@@ -113,7 +122,6 @@ impl Parser {
     fn parse_preamble(&mut self) {
         print!("Parsing WASM header ... ");
 
-        //let mut rdr = Cursor::new(file);
         let magic = self.read_uint32();
         if magic != MAGIC_NUM {
             panic!("Not a wasm file!");
@@ -127,7 +135,7 @@ impl Parser {
 
     fn parse_section(&mut self) -> bool {
         // this is the only way to check for EOF that I know of!
-        let sec_id = match self.read_varuint_len(7) {
+        let sec_id = match self.read_varuint_impl(7) {
             Some((val, _)) => val,
             None => return false,
         };
@@ -136,7 +144,7 @@ impl Parser {
         let mut name_offset: u64 = 0;
         let mut name: String = String::new();
         if sec_id == 0 {
-            let (name_len, name_len_field_size) = self.read_varuint_len(32).unwrap();
+            let (name_len, name_len_field_size) = self.read_varuint_impl(32).unwrap();
             name_offset = (name_len_field_size as u64) + name_len;
             let name_bytes = self.read_bytes(name_len);
             name = String::from_utf8(name_bytes).unwrap();
@@ -157,7 +165,7 @@ impl Parser {
             }
             0x1 => self.parse_section_todo(payload_data_len),
             0x2 => self.parse_section_todo(payload_data_len),
-            0x3 => self.parse_section_todo(payload_data_len),
+            0x3 => self.parse_function_section(payload_data_len),
             0x4 => self.parse_section_todo(payload_data_len),
             0x5 => self.parse_section_todo(payload_data_len),
             0x6 => self.parse_section_todo(payload_data_len),
@@ -176,13 +184,13 @@ impl Parser {
     fn parse_section_todo(&mut self, payload_len: u64) {
         println!("  # Parsing section (TODO)");
         self.read_bytes(payload_len);
-        println!("  + Parsing section (TODO) done")
+        println!("  + Parsing section (TODO) done");
     }
 
     fn parse_name_custom_section(&mut self, payload_len: u64) {
         println!("  # Parsing name custom section");
         let payload = self.read_bytes(payload_len);
-        println!("  + Parsing name custom section done")
+        println!("  + Parsing name custom section done");
     }
 
     fn parse_custom_section(&mut self, name: &str, payload_len: u64) {
@@ -195,6 +203,21 @@ impl Parser {
         );
         println!("  + Parsing custom section done")
         // return name, payload
+    }
+
+    fn parse_function_section(&mut self, payload_len: u64) {
+        println!("  # Parsing function section");
+        let init_offset = self.get_current_offset();
+        let count = self.read_varuint(32);
+        let mut types = Vec::<u32>::new();
+        for _ in 0..count {
+            let t = self.read_varuint(32) as u32;
+            types.push(t);
+        }
+        assert!(self.get_read_len(init_offset) == payload_len);
+        //println!("{}", types);
+        println!("  + Parsing function section done");
+        // return types
     }
 }
 
